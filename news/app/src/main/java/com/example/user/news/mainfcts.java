619e19e2f50d4f523486d6fa47d5b500124e472e
maria19988 <31484675+maria19988@users.noreply.github.com>
2018-03-10 22:53:37 +0200

    layout

---
+++ news/app/src/main/java/com/example/user/news/mainfcts.java
@@ -0,0 +1,11 @@
+package com.example.user.news;
+
+/**
+ * Created by user on 3/10/2018.
+ */
+
+public class mainfcts //in this class we are going to put our main functions like checking whether there is wifi
+{
+
+
+}
